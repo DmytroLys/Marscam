cd42fe6712446554d3988887329cb2b22027c598
Dmytro Lyshtva <lyshtva.dmytro@gmail.com>
2023-09-20 17:04:14 +0200

    Create LaunchViewController with animation

---
+++ Marscam/Controller/LaunchViewController.swift
@@ -0,0 +1,76 @@
+//
+//  LaunchViewController.swift
+//  Marscam
+//
+//  Created by Dmytro Lyshtva on 20.09.2023.
+//
+
+import UIKit
+import Lottie
+
+class LaunchViewController: UIViewController {
+    
+    
+    var logoView: UIView!
+    private var animationView: LottieAnimationView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureLogoView()
+        configureAnimation()
+        print("Loaded")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        Timer.scheduledTimer(withTimeInterval: 10.0, repeats: false) { timer in
+            self.navigateToMainViewController()
+        }
+        
+    }
+    
+    func configureLogoView() {
+        logoView = UIView()
+        view.addSubview(logoView)
+        
+        logoView.translatesAutoresizingMaskIntoConstraints = false
+        logoView.widthAnchor.constraint(equalToConstant: 123).isActive = true
+        logoView.heightAnchor.constraint(equalToConstant: 123).isActive = true
+        logoView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logoView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        logoView.layer.cornerRadius = 30
+        logoView.backgroundColor = UIColor(named: "accentOne")
+    }
+    
+    func configureAnimation() {
+        
+        animationView = LottieAnimationView(name: "Loader_Test")
+        animationView!.contentMode = .scaleAspectFill
+        animationView!.loopMode = .loop
+        animationView!.animationSpeed = 1.0
+        view.addSubview(animationView!)
+        
+        animationView!.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        animationView?.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -114).isActive = true
+        animationView?.heightAnchor.constraint(equalToConstant: 34).isActive = true
+        animationView?.widthAnchor.constraint(equalToConstant: 222).isActive = true
+        
+        animationView?.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        
+        animationView!.play()
+    }
+    
+    func navigateToMainViewController() {
+        let mainVC = ViewController()
+        mainVC.modalPresentationStyle = .fullScreen
+        mainVC.modalTransitionStyle = .crossDissolve
+        self.present(mainVC, animated: true, completion: nil)
+    }
+    
+    
+    
+}
